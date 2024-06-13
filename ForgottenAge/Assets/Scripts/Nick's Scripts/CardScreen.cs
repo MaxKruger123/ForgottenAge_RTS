a0f0649a511534affd0b87bc33bf131a46d5afa5
NicholasIsherwood <2465008@students.wits.ac.za>
2024-06-13 18:22:01 +0200

    Card data

--- ForgottenAge/Assets/Scripts/Nick's Scripts/CardScreen.cs
+++ ForgottenAge/Assets/Scripts/Nick's Scripts/CardScreen.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class CardScreen : MonoBehaviour
 {
     public WaveManager waveManagera;
-    
+
+    public List<CardData> cardDataList = new List<CardData>();
+    public List<GameObject> drawnCards = new List<GameObject>();
+    public List<Sprite> cardImages = new List<Sprite>();
+
+    public Image image1;
+    public Image image2;
+    public Image image3;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateCardDataSet();
     }
 
     // Update is called once per frame
@@ -21,6 +32,7 @@ public class CardScreen : MonoBehaviour
     public void ShowScreen()
     {
         gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        DrawFromDeck();
         HideIconButton();
     }
 
@@ -40,4 +52,77 @@ public class CardScreen : MonoBehaviour
     {
         gameObject.transform.GetChild(1).gameObject.SetActive(false);
     }
+
+    public void CreateCardDataSet()
+    {
+        // Title, bufftype, buffvalue, debuff, debuffvalue, sprite, description
+        cardDataList.Add(new CardData("Concentration", "Concentration Per Second", 5, "", 0, cardImages[0], "Gain extra concentration per second"));
+
+        cardDataList.Add(new CardData("Glass Cannon", "Ally Melee Damage", 2, "Ally Troop Health", 2, cardImages[0], "Your troops do more damage but are weaker"));
+
+        cardDataList.Add(new CardData("The Dreamer", "Dream Token", 1, "", 0, cardImages[0], "Gain one dream token"));
+
+        cardDataList.Add(new CardData("Focus", "Concentration", 100, "All Troop Cost", 5, cardImages[0], ""));
+
+        cardDataList.Add(new CardData("Dreamer's Resolve", "Dream Token", 3, "Destroy Random Building", 2, cardImages[0], ""));
+
+        cardDataList.Add(new CardData("Perilous Insight", "Concentration Per Second", 3, "Enemy damage multiply", 2, cardImages[0], ""));
+
+        cardDataList.Add(new CardData("Fortified Respite", "Passive Tank Healing", 0, "Ally Ranged Reload", -1, cardImages[0], ""));
+
+        cardDataList.Add(new CardData("Cursed Blessing", "Ally Damage", 2, "Memory Tile Are Lost", 2, cardImages[0], ""));
+
+        cardDataList.Add(new CardData("Neuron Activation", "Concentration", 400, "Skip Wave", 3, cardImages[0], ""));
+
+        cardDataList.Add(new CardData("Shifting Tides", "Gain Memory Tile", 2, "Chance Allies Become Enemies", 0, cardImages[0], ""));
+
+        cardDataList.Add(new CardData("Cognitive Fortification", "Allies Health", 20, "", 0, cardImages[0], "%"));
+
+        cardDataList.Add(new CardData("Structural Recall", "Heal All Buidlings", 1, "", 0, cardImages[0], ""));
+
+        cardDataList.Add(new CardData("Synaptic Overload", "Defense Tower Damage", 2, "", 0, cardImages[0], ""));
+
+    }
+
+    public void DrawFromDeck()
+    {
+        int randNum;
+
+        for(int i = 0; i < 3; i++)
+        {
+            randNum = Random.Range(0, cardDataList.Count);
+            drawnCards[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = cardDataList[randNum].title; // sets drawn card title to the randomly selected cards title
+            drawnCards[i].transform.GetChild(1).GetComponent<Image>().sprite = cardDataList[randNum].image; // sets drawn card image to the randomly selected cards image
+            drawnCards[i].transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = cardDataList[randNum].descriptionText;
+
+            // buff
+            drawnCards[i].transform.GetChild(3).GetComponent<TextMeshProUGUI>().text = cardDataList[randNum].buffType;
+            // if there is no buff it wont display anything for it
+            if (cardDataList[randNum].buff == 0)
+            {
+                drawnCards[i].transform.GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
+            }
+            else
+            {
+                drawnCards[i].transform.GetChild(3).GetChild(0).GetComponent<TextMeshProUGUI>().text = cardDataList[randNum].buff.ToString();
+            }
+            
+            // debuff
+            drawnCards[i].transform.GetChild(4).GetComponent<TextMeshProUGUI>().text = cardDataList[randNum].debuffType;
+            // if there is no debuff it wont display anything for it
+            if (cardDataList[randNum].debuff == 0)
+            {
+                drawnCards[i].transform.GetChild(4).GetChild(0).GetComponent<TextMeshProUGUI>().text = "";
+            }
+            else
+            {
+                drawnCards[i].transform.GetChild(4).GetChild(0).GetComponent<TextMeshProUGUI>().text = cardDataList[randNum].debuff.ToString();
+            }
+                
+            
+        }
+    }
+
+
+    
 }
